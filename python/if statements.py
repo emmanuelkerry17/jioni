23c065550e8dad5f60cf14389839413b67f66b86
ms-chanky <naomi.bowen06@gmail.com>
2024-02-08 16:29:40 +0300

    message

---
+++ python/if statements.py
@@ -0,0 +1,14 @@
+# english = 60
+# if english > 40 :
+#     print("english is over 40")
+# elif english < 40:
+#     print("english is below 40")
+#
+# else:
+#     print("english  equal to 40")
+
+maths = 64
+# if maths >=70 and maths <=100:
+#     print("A")
+if maths < 70 or maths > 80:
+    print("condition met")
